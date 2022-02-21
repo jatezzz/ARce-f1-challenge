e0467dcb99999d12e30246f48663ebe9c562dc6b
John Trujillo <johnandrestrujillo@gmail.com>
2022-02-21 10:55:05 -0500

    ObjectInRace fixed to be a generic type.

--- Shared/Data Models/LapDataModel.swift
+++ Shared/Data Models/LapDataModel.swift
@@ -42,16 +42,13 @@ final class LapDataModel: ObservableObject {
         let carScene = try! COTA.loadTrack()
 
         // Hidding the reference track
-        let trackOfReference = carScene.track3!
-        trackOfReference.isEnabled = false
+        let trackDefaultOnMap = carScene.track3!
+        trackDefaultOnMap.isEnabled = false
 
         // • Loading the nice track from the usdc file
-        let myTrackTransformed = try! Entity.load(named: "1960Final")
+        let historicalTrack = try! Entity.load(named: "1960Final")
 
-
-        myTrackTransformed.orientation = simd_quatf(angle: .pi / 4, axis: [0, 1, 0])
-
-//        installGestures(on: trackAnchor)
+        historicalTrack.orientation = simd_quatf(angle: .pi / 4, axis: [0, 1, 0])
 
         // • The camera
         let cameraEntity = PerspectiveCamera()
@@ -62,56 +59,43 @@ final class LapDataModel: ObservableObject {
         cameraAnchor.addChild(cameraEntity)
         #endif
 
-
         // • The car
         let myCar = carScene.car!
         let trackingCone = carScene.trackingCone!
 
         myCar.transform.scale = [1, 1, 1] * 0.0008
-
-        // • Reference car
-        let trackingCone2 = trackingCone.clone(recursive: true)
-        let fastestCar = myCar.clone(recursive: true)
-        fastestCar.addChild(trackingCone2)
-        carScene.addChild(fastestCar)
-        myCar.addChild(trackingCone)
+        myCar.isEnabled = false
+        trackingCone.isEnabled = false
 
         // Initially position the camera
         #if os(macOS)
-        cameraEntity.look(at: trackOfReference.position, from: [0, 50, 0], relativeTo: nil)
+        cameraEntity.look(at: trackDefaultOnMap.position, from: [0, 50, 0], relativeTo: nil)
         #endif
 
         // Run the car
-        mainCar = ObjectInRace(entity: myCar, camera: cameraEntity, referenceEntityTransform: myTrackTransformed, referenceEntity: trackOfReference)
+        mainCar = ObjectInRace(referenceModel: myCar, camera: cameraEntity, container: historicalTrack, referenceCone: trackingCone)
 
-        secondCar = ObjectInRace(entity: fastestCar, camera: nil, referenceEntityTransform: myTrackTransformed, referenceEntity: trackOfReference)
+        secondCar = ObjectInRace(referenceModel: myCar, camera: nil, container: historicalTrack, referenceCone: trackingCone)
 
         #if !os(macOS)
-        let container = createBox(size: 0.4)
-        placeBox(box: container, at: SIMD3(x: 0, y: 0, z: 0))
-        container.addChild(myTrackTransformed)
+
+        let container = createBox(size: 0.001)
+        placeBox(box: container, at: SIMD3.zero)
+        container.addChild(historicalTrack)
         container.generateCollisionShapes(recursive: true)
         arView.installGestures([.all], for: container).forEach {
             $0.addTarget(self, action: #selector(handleModelGesture))
         }
-        arView.debugOptions = [
-//            .showPhysics,
-//                                .showStatistics,
-                                .showWorldOrigin,
-                                .showAnchorOrigins,
-//                                .showAnchorGeometry,
-//                                .showFeaturePoints,
-//                                .showSceneUnderstanding
-        ]
+        arView.debugOptions = [.showWorldOrigin, .showAnchorOrigins]
         #endif
-        
+
         let trackAnchor = AnchorEntity(world: .zero)
         #if !os(macOS)
         trackAnchor.addChild(container)
         #else
-        trackAnchor.addChild(myTrackTransformed)
+        trackAnchor.addChild(historicalTrack)
         #endif
-        
+
         #if !targetEnvironment(simulator) && !os(macOS)
         arView.addCoaching()
         #endif
@@ -120,23 +104,13 @@ final class LapDataModel: ObservableObject {
         #if os(macOS)
         arView.scene.addAnchor(cameraAnchor)
         #endif
-        
-        
-        let planeMesh = MeshResource.generatePlane(width: 0.15, depth: 0.15)
-        let planeMaterial = SimpleMaterial(color: .white, isMetallic: false)
-        let planeEntity : ModelEntity? = ModelEntity(mesh: planeMesh, materials: [planeMaterial])
-        let planeAnchor = AnchorEntity()
-        planeAnchor.addChild(planeEntity!)
-        arView.scene.addAnchor(planeAnchor)
-//        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(tapOnARView))
-//        arView.addGestureRecognizer(tapGesture)
-        
+
         sceneEventsUpdateSubscription = arView.scene.subscribe(to: SceneEvents.Update.self) { [weak self] _ in
             guard let self = self else {
                 return
             }
             self.objects.indices.forEach({
-                let viewData = self.objects[$0].update()
+                let viewData = self.objects[$0].updateAndRetrieveViewData()
                 if let viewData = viewData, $0 == 0 {
                     self.mainParticipant = viewData
                 }
@@ -144,134 +118,40 @@ final class LapDataModel: ObservableObject {
                     self.secondarticipant = viewData
                 }
             })
-            
-//            #if !os(macOS)
-//            guard let result = self.arView.raycast(from: self.arView.center, allowing: .estimatedPlane, alignment: .horizontal).first else {
-//            return
-//
-//            }
-//            planeEntity!.setTransformMatrix(result.worldTransform, relativeTo: nil)
-//            #endif
-            
         }
     }
-    
-    var gestureStartLocation:SIMD3<Float>?
 
     #if !os(macOS)
     @objc func handleModelGesture(_ sender: Any) {
         switch sender {
         case let rotation as EntityRotationGestureRecognizer:
             print("Rotation and name:\(rotation.entity!.name)")
-//            rotation.isEnabled = false
         case let translation as EntityTranslationGestureRecognizer:
             print("translation and nane \(translation.entity!.name)")
-            if translation.state == .ended || translation.state == .cancelled {
-                gestureStartLocation = nil
-                return
-            }
-            guard let gestureCurrentLocation = translation.entity?.transform.translation else { return }
-            guard let _ = gestureStartLocation else {
-                gestureStartLocation  = gestureCurrentLocation
-                return
-            }
-            let delta = gestureStartLocation! - gestureCurrentLocation
-            let distance = ((delta.x * delta.x) + (delta.y * delta.y) + (delta.z + delta.z)).squareRoot()
-            print("startlocation;\(String(describing:gestureStartLocation)), currentlocation: \(gestureCurrentLocation), the distance is \(distance)")
-            
-        case let Scale as EntityScaleGestureRecognizer:
-//            Scale.removeTarget(nil, action: nil)
-//            Scale.addTarget(self, action: #selector(handleScaleGesture))
+        case let scale as EntityScaleGestureRecognizer:
             print("In Scale")
         default:
             break
         }
     }
-    @objc func handleScaleGesture(_ sender:Any){
-       print("In Scale")
-    }
-    
-    @objc func tapOnARView(sender: UITapGestureRecognizer) {
-        guard let arView = arView else { return }
-        let location = sender.location(in: arView)
-//        if let node = nodeAtLocation(location) {
-//            removeCircle(node: node)
-//        }
-//        else
-        if let result = raycastResult(fromLocation: location) {
-            addCircle(raycastResult: result)
-        }
-    }
-
-
-    private var circles: [Entity] = []
-    private func raycastResult(fromLocation location: CGPoint) -> CollisionCastHit? {
-        // let query = arView.makeRaycastQuery(from: location,
-//    allowing: .existingPlaneGeometry,
-//    alignment: .horizontal),
-        guard let ray = self.arView.ray(through: location) else { return nil }
-        
-        let results = arView.scene.raycast(origin: ray.origin, direction: ray.direction)
-//        let results = arView.session.raycast(query)
-        print(results)
-        return results.first
-    }
-    
-    private func addCircle(raycastResult: CollisionCastHit) {
-        let circleNode = GeometryUtils.createSphere()
-        if circles.count >= 2 {
-            for circle in circles {
-                circle.removeFromParent()
-            }
-            circles.removeAll()
-        }
-        
-        let planeAnchor = AnchorEntity(world: raycastResult.position)
-        planeAnchor.addChild(circleNode)
-        arView.scene.addAnchor(planeAnchor)
-//        circleNode.setTransformMatrix(raycastResult.position, relativeTo: nil)
-        
-        circles.append(circleNode)
-        nodesUpdated()
-    }
-
-
-    private func nodesUpdated() {
-        if circles.count == 2 {
-            let distance = GeometryUtils.calculateDistance(firstNode: circles[0], secondNode: circles[1])
-            print("distance = \(distance)")
-        }
-    }
-
     #endif
 
     func load(session: Session) {
         objects.removeAll()
-
-//        let box2 = createBox()
-//        placeBox(box: box2, at: SIMD3(x: 0, y: 0, z: 0))
-//        installGestures(on: box2)
-
         loadSessionIntoModel(session: session, model: mainCar)
     }
 
-    func createBox(size: Float = 0.08) -> ModelEntity{
+    func createBox(size: Float = 0.08) -> ModelEntity {
         let box = MeshResource.generateBox(size: size) // Generate mesh
         let boxMaterial = SimpleMaterial(color: .blue, isMetallic: true)
         let boxEntity = ModelEntity(mesh: box, materials: [boxMaterial])
         return boxEntity
     }
-    func placeBox(box:ModelEntity,at position: SIMD3<Float>){
+
+    func placeBox(box: ModelEntity, at position: SIMD3<Float>) {
         let boxAnchor = AnchorEntity(world: position)
         boxAnchor.addChild(box)
         arView.scene.addAnchor(boxAnchor)
-    }
-    
-    func installGestures(on object: ModelEntity){
-        object.generateCollisionShapes(recursive: true)
-        #if !os(macOS)
-        arView.installGestures([.rotation,.scale], for: object)
-        #endif
     }
 
     func addSession(session: Session) {
@@ -282,7 +162,7 @@ final class LapDataModel: ObservableObject {
         AppModel.shared.appState = .loadingTrack
         self.cancellable = []
         model.reset()
-        
+
         NetworkHelper.shared.fetchPositionData(for: session)
                 .receive(on: RunLoop.main)
                 .sink { completion in
--- Shared/Data Models/ObjectInRace.swift
+++ Shared/Data Models/ObjectInRace.swift
@@ -14,22 +14,21 @@ class ObjectInRace {
     var positionList: [Motion] = []
     let mainEntity: Entity
     let cameraEntity: PerspectiveCamera?
-    let referenceEntity: Entity
-    let referenceEntityTransform: Entity
-    let coneEntity: Entity?
+    let coneEntity: Entity
 
-    init(entity: Entity, camera: PerspectiveCamera?, referenceEntityTransform: Entity, referenceEntity: Entity) {
-        mainEntity = entity
-        coneEntity = mainEntity.children.first(where: {
-            $0.name == "Tracking Cone"
-        })
-        coneEntity?.transform.scale = [1, 1, 1] * 200
-        coneEntity?.setPosition(SIMD3<Float>([0, 50, 0]), relativeTo: mainEntity)
+    init(referenceModel: Entity, camera: PerspectiveCamera?, container: Entity, referenceCone: Entity) {
+        mainEntity = referenceModel.clone(recursive: true)
+        coneEntity = referenceCone.clone(recursive: true)
+        coneEntity.isEnabled = true
+        mainEntity.addChild(coneEntity)
+        container.addChild(mainEntity)
+        mainEntity.setPosition(SIMD3.zero, relativeTo: nil)
+        mainEntity.isEnabled = true
 
-        self.cameraEntity = camera
-        self.referenceEntityTransform = referenceEntityTransform
-        self.referenceEntity = referenceEntity
-        print("TEST-referenceEntityTransform: \(referenceEntityTransform)")
+        coneEntity.transform.scale = [1, 1, 1] * 200
+        coneEntity.setPosition(SIMD3<Float>([0, 50, 0]), relativeTo: mainEntity)
+
+        cameraEntity = camera
     }
 
     func reset() {
@@ -37,19 +36,14 @@ class ObjectInRace {
         currentFrame = 0
     }
 
-    func update() -> ParticipantViewData? {
-        guard AppModel.shared.appState == .playing, !self.positionList.isEmpty, let coneEntity = coneEntity else { return nil }
+    func updateAndRetrieveViewData() -> ParticipantViewData? {
+        guard AppModel.shared.appState == .playing, !self.positionList.isEmpty else { return nil }
 
-        print("TEST-mainEntity.transform B: \(mainEntity.transform)")
         let cp = self.positionList[self.currentFrame]
         mainEntity.position = SIMD3<Float>([cp.mWorldposy, cp.mWorldposz, cp.mWorldposx] / 1960)
         mainEntity.transform.rotation = Transform(pitch: cp.mPitch, yaw: cp.mYaw, roll: cp.mRoll).rotation
 
-        print("TEST-mainEntity.transform C: \(mainEntity.transform)")
         // converting the API coordinates to match the visible track
-        mainEntity.transform = referenceEntityTransform.convert(transform: mainEntity.transform, to: referenceEntity)
-        print("TEST-mainEntity.transform L: \(mainEntity.transform)")
-
         #if os(macOS)
         cameraEntity?.look(at: mainEntity.position, from: [0.1, 0.1, 0], relativeTo: nil)
         #endif
